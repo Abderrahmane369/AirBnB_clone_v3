3d13ecd2947deac78835a8a3ab016198e3924bd8
Abderrahmane369 <cszamasu@gmail.com>
2024-02-03 00:44:09 +0100

    Updated_Something

---
+++ tests/test_api/test_v1/test_app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""loo"""
+import unittest
+
+
+class AppTest(unittest.case):
+    """APP TESTING"""
+    pass
---
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""loo"""
+import unittest
+
+
+class IndexTest(unittest.case):
+    """APP TESTING"""
+    pass
