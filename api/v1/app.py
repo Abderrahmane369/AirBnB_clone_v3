ca7c0d1386b821113161d938de7ed0da50df7e40
Abderrahmane369 <cszamasu@gmail.com>
2024-02-02 23:06:56 +0100

    Updated_Something

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,8 +2,8 @@
 """Application"""
 from flask import Flask
 from models import storage
+from api.v1.views import app_views
 from os import getenv
-from api.v1.views.index import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """index"""
 from api.v1.views import app_views
-from flask import Blueprint
 
 
 @app_views.route('/status')
 def status():
     """aaaaa"""
-    return {
-        'status': 'OK'
-    }
+    return {'status': 'OK'}
