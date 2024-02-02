c95d9a04e11d7d298f12fb2d391a10e655b7bace
Abderrahmane369 <cszamasu@gmail.com>
2024-02-02 22:09:50 +0100

    Updated_Something

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """index"""
 from api.v1.views import app_views
+from flask import Blueprint
 
 
 @app_views.route('/status')
 def status():
-    """status"""
-    return {'status': 'OK'}
+    """aaaaa"""
+    return {
+        'status': 'OK'
+    }
