13444a43e6c6585234a0171dab5d4769a1a09a7c
Abderrahmane369 <cszamasu@gmail.com>
2024-02-03 01:13:40 +0100

    Updated_Something

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,10 @@ from flask import Blueprint, jsonify
 app_views = Blueprint('app_views', __name__,
                       url_prefix='/api/v1')
 
-
+@app_views.route('/')
+def main():
+    """aaa"""
+    return "hi"
 @app_views.route('/status')
 def status():
     """aaaaa"""
