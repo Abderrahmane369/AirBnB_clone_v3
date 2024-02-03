24ade2b5b4d219568cb563532f98b02969c2008c
Abderrahmane369 <cszamasu@gmail.com>
2024-02-03 15:06:36 +0100

    Updated_Something

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ app_views = Blueprint('app_views', __name__,
                       url_prefix='/api/v1')
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
     """aaaaa"""
     return jsonify({'status': 'OK'})
