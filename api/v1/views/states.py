29900a725f72a95dc6ba2d4bce43c52fa02c64b3
Abderrahmane369 <cszamasu@gmail.com>
2024-02-03 18:30:53 +0100

    Updated_Something

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 """loooog"""
 from models import storage
-from fla
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'],
+                 strict_slashes=False)
+def states():
+    """states"""
+    return list(map(lambda v: v.to_dict(),
+                    storage.all(State).values()))
+
+
+@app_views.route('/state/<state_id>', methods=['GET'],
+                 strict_slashes=False)
+def state(state_id):
+    """state"""
+    return storage.get(State, state_id).to_dict() || 404
