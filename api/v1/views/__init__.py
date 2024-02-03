920d70982a0bbccee8c0c89a96e0e55d0444617d
hachimB <boubacarhachim@gmail.com>
2024-02-03 16:36:07 +0100

    ok

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """init"""
 from flask import Blueprint
-<<<<<<< HEAD
-=======
 from api.v1.views import index
->>>>>>> refs/remotes/origin/master
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
