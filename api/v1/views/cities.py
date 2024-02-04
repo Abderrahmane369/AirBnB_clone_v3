36c2b8466db3091760e047daab1398b6c09503a1
hachimB <boubacarhachim@gmail.com>
2024-02-04 12:26:50 +0100

    ok

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -15,7 +15,7 @@ def cities(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    return jsonify([city.to_dict() for city in state.city])
+    return [city.to_dict() for city in state.city]
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
