d23e7e9f8f2c576ad63c8365bdac3e7abd7ea084
hachimB <boubacarhachim@gmail.com>
2024-02-04 12:23:00 +0100

    ok

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -24,7 +24,7 @@ def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    return jsonify(city.to_dict())
+    return city.to_dict()
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
@@ -35,7 +35,7 @@ def del_city(city_id):
         abort(404)
     storage.delete(city)
     storage.save()
-    return jsonify({}), 200
+    return {}, 200
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
@@ -52,7 +52,7 @@ def create_city(state_id):
         abort(400, "Missing name")
     city = City(name=body_request['name'], state_id=state_id)
     city.save()
-    return jsonify(city.to_dict()), 201
+    return city.to_dict(), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
@@ -69,4 +69,4 @@ def update_city(city_id):
         if key not in ignored_keys:
             setattr(city, key, value)
    city.save()
-    return jsonify(city.to_dict()), 200
+    return city.to_dict(), 200
