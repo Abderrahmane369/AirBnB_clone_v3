2945bd97cb0dccbf24743571c3027cd1a187e5de
Abderrahmane369 <cszamasu@gmail.com>
2024-02-04 18:19:47 +0100

    Updated_Something

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -17,64 +17,57 @@ def states():
 
 @app_views.route('/states/<state_id>', methods=['GET'],
                  strict_slashes=False)
-def getState(state_id):
+def handleStateRequest(state_id):
     """get"""
-    if storage.get(State, state_id):
-        return storage.get(State, state_id).to_dict()
-    abort(404)
-
-
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def deleteState(state_id):
-    """delete"""
-    if storage.get(State, state_id):
-        storage.delete(storage.get(State, state_id))
-        storage.save()
-        return {}, 202
-
-    abort(404)
-
-
-@app_views.route('/states/', methods=['POST'],
-                 strict_slashes=False)
-def createState():
-    """create"""
-    data = request.get_json()
-    state = State(**data)
-
-    if not data:
-        abort(400, description='Not a JSON')
-    elif 'name' not in data:
-        abort(400, description='Missing name')
-
-    storage.new(state)
-    storage.save()
-
-    return state.to_dict(), 201
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def updateState(state_id):
-    """update"""
-    data = request.get_json()
-    state = storage.get(State, state_id)
-
-    if not data:
-        abort(400, description='Not a JSON')
-    elif not state:
+    if request.method == 'GET':
+        if storage.get(State, state_id):
+            return storage.get(State, state_id).to_dict()
         abort(404)
 
-    icu = {'id', 'created_at', 'updated_at'}
+    """delete"""
+    if request.method == 'DELETE':
+        if storage.get(State, state_id):
+            storage.delete(storage.get(State, state_id))
+            storage.save()
+            return {}, 202
 
-    storage.delete(state)
+        abort(404)
 
-    for a, v in data.items():
-        if hasattr(state, a) and a not in icu:
-            setattr(state, a, v)
+    """create"""
+    if request.method == 'POST':
+        data = request.get_json()
+        state = State(**data)
 
-    storage.new(state)
-    storage.save()
+        if not data:
+            abort(400, description='Not a JSON')
+        elif 'name' not in data:
+            abort(400, description='Missing name')
 
-    return state.to_dict(), 200
+        storage.new(state)
+        storage.save()
+
+        return state.to_dict(), 201
+
+
+    """update"""
+    if request.method == 'PUT':
+        data = request.get_json()
+        state = storage.get(State, state_id)
+
+        if not data:
+            abort(400, description='Not a JSON')
+        elif not state:
+            abort(404)
+
+        icu = {'id', 'created_at', 'updated_at'}
+
+        storage.delete(state)
+
+        for a, v in data.items():
+            if hasattr(state, a) and a not in icu:
+                setattr(state, a, v)
+
+        storage.new(state)
+        storage.save()
+
+        return state.to_dict(), 200
