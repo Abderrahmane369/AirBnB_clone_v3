eba3f659a3ce4f5f7a6d787005691ef759c17cdb
Abderrahmane369 <cszamasu@gmail.com>
2024-02-06 23:32:59 +0100

    Updated_Something

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from api.v1.views import app_views
 from models.place import Place
 from models import storage
+from models import storage_t
 from models.amenity import Amenity
 
 
@@ -30,43 +31,21 @@ def delete_amenity(place_id, amenity_id):
     if amenity not in place.amenities:
         abort(404)
 
-    storage.delete(amenity)
+    place.amenities.remove(amenity)
     storage.save()
     return jsonify({}),  200
 
 
-@app_views.route('/places/<place_id>/amenities',
+@app_views.route('places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
-def create_amenity(place_id):
+def create_amenity(place_id, amenity_id):
     """Create a new amenity for a place"""
-    body_request = request.get_json()
-    if not body_request:
-        abort(400, "Not a JSON")
-    if 'name' not in body_request:
-        abort(400, "Missing name")
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-
-    body_request['place_id'] = place_id
-    amenity = Amenity(**body_request)
-    amenity.save()
-    return jsonify(amenity.to_dict()),  201
-
-
-@app_views.route('/amenities/<amenity_id>',
-                 methods=['PUT'], strict_slashes=False)
-def update_amenity(amenity_id):
-    """Update a specific amenity"""
     amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
+    place = storage.get(Place, place_id)
+
+    if not amenity or not place:
         abort(404)
-    body_request = request.get_json()
-    if not body_request:
-        abort(400, "Not a JSON")
-    ignored_keys = ['id', 'created_at', 'updated_at', 'place_id']
-    for key, value in body_request.items():
-        if key not in ignored_keys:
-            setattr(amenity, key, value)
-    amenity.save()
-    return jsonify(amenity.to_dict()),  200
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
+
+    return jsonify(place.amenities.append(amenity)),  201
