8e6e5528f990ffdb32ea150e95434f869cdd333c
Abderrahmane369 <cszamasu@gmail.com>
2024-02-06 01:41:00 +0100

    Updated_Something

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,9 +9,5 @@ from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-<<<<<<< HEAD
 from api.v1.views.places_reviews import *
 from api.v1.views.index import *
-=======
-from api.v1.views.index import *
->>>>>>> refs/remotes/origin/master
